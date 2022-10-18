3b539a78c8f9d8409640dc2bbff49def60e0be19
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-18 14:32:37 -0700

    petgram

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable prefer-regex-literals */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  stats: { children: true },
+  entry: {
+    // Punto de entrada
+    main: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    // Punto de salida
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' })
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env'],
+            [
+              '@babel/preset-react',
+              {
+                runtime: 'automatic'
+              }
+            ]
+          ],
+          plugins: ['@emotion']
+        }
+      }
+    ]
+  }
+}
