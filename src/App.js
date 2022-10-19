427247a7fe770c0f7b0c2694f9363b3e30b923d4
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-18 19:51:28 -0700

    [ADD] create component categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import Category from './components/Category'
+import { GlobalStyles } from './styles/GlobalStyles'
+import ListOfCategories from './components/ListOfCategories'
 
 export default function App () {
   return (
-    <Category />
+    <>
+      <GlobalStyles />
+      <ListOfCategories />
+    </>
   )
 }
--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
-export default function Category ({ cover = DEFAULT_IMAGE }) {
+export default function Category ({
+  path = '#',
+  cover = DEFAULT_IMAGE,
+  emoji = '🐶'
+}) {
   return (
-    <div>
-      Category
-      {/* <img src={cover} alt='logo' /> */}
-    </div>
+    <Link to={path}>
+      <Image src={cover} alt={emoji} />
+      {emoji}
+    </Link>
+
   )
 }
