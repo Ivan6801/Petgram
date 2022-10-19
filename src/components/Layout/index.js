0cc55c59b3aca34c78ed6d1538d229b5ce54a860
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-18 20:18:51 -0700

    [UPDATE] layout to home

---
+++ src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { Div, Title, Subtitle } from './styles'
+import { MdPets } from 'react-icons/md'
+
+export const Layout = ({ children, title, subtitle, verDatos = true }) => {
+  return (
+    <>
+      <Helmet>
+        {title && <title>{title} | Petgram 🐶</title>}
+        {subtitle && <meta name='description' content={subtitle} />}
+
+      </Helmet>
+      <Div>
+        {verDatos && <span>{title && <Title>{title} <MdPets style={{ marginLeft: '5px' }} /> </Title>}</span>}
+        {verDatos && <span>{subtitle && <Subtitle>{subtitle}</Subtitle>}</span>}
+        {children}
+      </Div>
+    </>
+  )
+}
