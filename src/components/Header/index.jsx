b3a2a6962f10b35a3b47f48523eb94008aa6e90f
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-20 16:41:34 -0700

    Esponal es ingles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
+export default function Header () {
+  const [t, i18n] = useTranslation('global')
+
+  return (
+    <div>
+      <h1>{t('header.hello-world')}</h1>
+      <Button onClick={() => i18n.changeLanguage('es')} variant='contained'>ES</Button>
+      <Button onClick={() => i18n.changeLanguage('en')} variant='contained'>EN</Button>
+    </div>
+  )
+}
