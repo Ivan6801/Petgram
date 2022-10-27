cc3ae059dbf88306a530d8c10d27aa7465490ed2
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-26 22:00:10 -0700

    SVGR: de SVG a componente de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { GlobalStyles } from './styles/GlobalStyles'
 import AppRouter from './router/AppRouter'
+import Logo from './components/Logo'
 
 export default function App () {
   return (
     <>
       <GlobalStyles />
+      <Logo />
       <AppRouter />
     </>
   )
