a22b92975d2d56e0519c000a954e8e458f6573cf
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-27 22:56:09 -0700

    Usando Intersection Observer

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/jsx-key */
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
 export default function ListOfPhotoCards () {
   return (
     <ul>
-      {[1, 2, 3, 4, 5].map((id) => <PhotoCard key={id} />)}
+      {[...Array(300).keys()].map((id) => <PhotoCard key={id} />)}
     </ul>
   )
 }
