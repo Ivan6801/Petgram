7b66cc0701d750c508e984d8c65fe50bc047b51d
Ivan Gonzalez Gonzalez <60527338+Ivan6801@users.noreply.github.com>
2022-11-29 20:24:41 -0800

    Revert "Creando la ruta Home router"

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,49 +1,56 @@
-import React, { useState, useEffect, useRef } from "react";
-import Category from "../Category";
-import { List, Item } from "./styles";
-import useCategoriesData from "../../hooks/useCategoriesData";
+import React, { useState, useEffect, useRef } from 'react'
+import { CircularProgress, Box } from '@mui/material'
+import Category from '../Category'
+import { List, Item } from './styles'
+import useCategoriesData from '../../hooks/useCategoriesData'
 
 // https://pokeapi.co/api/v2/pokemon // https://petgram-serve-jorge-vicuna.vercel.app/categories
 
-export default function ListOfCategories() {
-  const { loading, categories } = useCategoriesData();
-  const [showFixed, setShowFixed] = useState(false);
-  const list = useRef(null);
+export default function ListOfCategories () {
+  const { loading, categories } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+  const list = useRef(null)
 
   useEffect(
     function () {
       const onScroll = (e) => {
-        const newShowFixed = window.scrollY > 200;
-        showFixed !== newShowFixed && setShowFixed(newShowFixed);
-      };
+        const newShowFixed = window.scrollY > 200
+        showFixed !== newShowFixed && setShowFixed(newShowFixed)
+      }
 
-      document.addEventListener("scroll", onScroll);
+      document.addEventListener('scroll', onScroll)
 
-      return () => document.removeEventListener("scroll", onScroll);
+      return () => document.removeEventListener('scroll', onScroll)
     },
     [showFixed]
-  );
+  )
 
   const renderList = (fixed) => (
-    <List ref={list} className={fixed ? "fixed" : ""}>
-      {loading ? (
-        <div>
-          <p>Loading...</p>
-        </div>
-      ) : (
-        categories.map((category) => (
-          <Item key={category.id}>
-            <Category {...category} path={`/pet/${category.id}`} />
-          </Item>
-        ))
-      )}
+    <List ref={list} className={fixed ? 'fixed' : ''}>
+      {loading
+        ? (
+          <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+          </Box>
+          )
+        : (
+            categories.map((category) => (
+              <Item key={category.id}>
+                <Category {...category} />
+              </Item>
+            ))
+          )}
     </List>
-  );
+  )
+
+  if (!loading) {
+    <p>Loading..</p>
+  }
 
   return (
     <>
       {renderList()}
       {showFixed && renderList(true)}
     </>
-  );
+  )
 }
