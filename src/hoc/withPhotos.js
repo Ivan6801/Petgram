1bd84115488486c14dbe1119731931f8284a22a7
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-30 21:25:41 -0800

    fix provider

--- src/hoc/withPhotos.js
+++ src/hoc/withPhotos.js
@@ -1,14 +1,17 @@
-import { gql  } from "@apollo/client";
+import { graphql } from 'react-apollo'
+import { gql } from '@apollo/client'
 
-export const withPhotos = gql`
-  query getPhotos($categoryId: ID) {
-    photos(categoryId: $categoryId) {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
+const GET_PHOTOS = gql`
+query getPhotos($categoryId: ID) {
+  photos(categoryId: $categoryId) {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
   }
-`;
+}
+`
+
+export const withPhotos = graphql(GET_PHOTOS)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-// import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "react-apollo";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
