8b72422b54527586ddd6dd75459d723bd6c1422b
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-12-01 11:03:40 -0800

    Rutas protegidas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
---
+++ src/pages/NotRegisteredUser.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotRegisteredUser() {
+  return (
+    <div>
+      <h1>NotRegisteredUser</h1>
+    </div>
+  );
+}
